9b54094098688307b008f6545885ab312b83657e
jx11r <ja11ralejandro@hotmail.com>
2021-12-27 11:25:11 -0600

    Remove old config

--- .config/qtile/modules/groups.py
+++ .config/qtile/modules/groups.py
@@ -7,33 +7,54 @@ from .keys import keys
 from .settings import mod
 
 # Workspaces
-workspaces = {
-    8: Group('   ', layout = 'max'),
-    4: Group('   ', layout = 'monadtall'),
-    2: Group('   ', layout = 'max', matches=[Match(wm_class=["firefox"])]),
-    1: Group('   ', layout = 'monadtall'),
-    3: Group('   ', layout = 'monadtall', matches=[Match(wm_class=["code"])]),
-    0: Group(' 阮  ', layout = 'monadwide', matches=[Match(wm_class=["spotify"])]),
-    9: Group(' 切  ', layout = 'monadtall', matches=[Match(wm_class=["telegram-desktop"])]),
-}
-
 groups = [
-    workspaces[i] for i in workspaces
-]
+    Group(
+        '1',
+        label = ''),
 
-def get_key(name):
-    return [
-        k for k,
-        g in workspaces.items()
-        if g.name == name
-    ][0]
+    Group(
+        '2',
+        label = ''),
+
+    Group(
+        '3',
+        label = ''),
+
+    Group(
+        '4',
+        label = ''),
+
+    Group(
+        '5',
+        label = ''),
+
+    Group(
+        '6',
+        label = ''),
+
+    Group(
+        '7',
+        label = ''),
+
+    Group(
+        '8',
+        label = ''),
+
+    Group(
+        '9',
+        label = ''),
+
+    Group(
+        '0',
+        label = ''),
+]
 
 # Navigation
 for i in groups:
     keys.extend([
         # Switch to group {}
-        Key([mod], str(get_key(i.name)), lazy.group[i.name].toscreen(), format(i.name)),
+        Key([mod], i.name, lazy.group[i.name].toscreen(), format(i.name)),
 
         # Move focused window to group {}
-        Key([mod, 'shift'], str(get_key(i.name)), lazy.window.togroup(i.name), format(i.name)),
+        Key([mod, 'shift'], i.name, lazy.window.togroup(i.name), format(i.name)),
     ])
