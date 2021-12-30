8a8b9581d69361fb4115ed688ba020248a346c9e
jx11r <ja11ralejandro@hotmail.com>
2021-12-29 23:43:07 -0600

    Update qtile bar

--- .config/qtile/modules/widgets.py
+++ .config/qtile/modules/widgets.py
@@ -428,14 +428,14 @@ def wttr(bg, fg):
 # Layouts
 main = [
     *logo(color[8], color[12]),
-
     sep(color[8]),
 
-    *cpu(color[8], color[17], color[2]),
+    *cpu(color[8], color[17], color[5]),
     sep('#'),
     *ram(color[8], color[17], color[3]),
-    sep('#'),
-    *disk(color[8], color[17], color[4]),
+
+    sep(color[8]),
+    *spotify(color[8], color[17], color[2]),
 
     spacer(None),
     *groups(None),
@@ -444,6 +444,10 @@ main = [
     *brightness(color[8], color[17], color[1]),
     sep('#'),
     *volume(color[8], color[17], color[2]),
+
+    sep(color[8]),
+    *weather(color[8], color[17], color[3]),
+
     sep(color[8]),
     *clock(color[8], color[17], color[4]),
 ]
