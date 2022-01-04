ca15e39bbe9515ed8a7594c0c8d37ed8d02465c0
jx11r <ja11ralejandro@hotmail.com>
2022-01-04 02:30:14 -0600

    Add new layouts & update key bindings

--- .config/qtile/modules/keys.py
+++ .config/qtile/modules/keys.py
@@ -6,20 +6,46 @@ from libqtile.lazy import lazy
 from .settings import browser, file_manager, mod, terminal
 
 keys = [
-    # Switch between windows
+    # Switch between windows [Xmonad & Bsp]
+    Key([mod], 'h', lazy.layout.left()),
+    Key([mod], 'l', lazy.layout.right()),
     Key([mod], 'j', lazy.layout.down()),
     Key([mod], 'k', lazy.layout.up()),
+    
+    # Next window [Xmonad]
     Key([mod], 'space', lazy.layout.next()),
 
-    # Move windows
+    # Move windows [Xmonad & Bsp]
     Key([mod, 'shift'], 'h', lazy.layout.shuffle_left()),
     Key([mod, 'shift'], 'l', lazy.layout.shuffle_right()),
     Key([mod, 'shift'], 'j', lazy.layout.shuffle_down()),
     Key([mod, 'shift'], 'k', lazy.layout.shuffle_up()),
 
-    # Grow windows
-    Key([mod, 'control'], 'h', lazy.layout.shrink()),
-    Key([mod, 'control'], 'l', lazy.layout.grow()),
+    # Flip windows [Bsp]
+    # Key([mod, 'mod1'], 'h', lazy.layout.flip_left()),
+    # Key([mod, 'mod1'], 'l', lazy.layout.flip_right()),
+    # Key([mod, 'mod1'], 'j', lazy.layout.flip_down()),
+    # Key([mod, 'mod1'], 'k', lazy.layout.flip_up()),
+
+    # Flip windows [Xmonad]
+    Key([mod, 'shift'], 'space', lazy.layout.flip()),
+
+    # Grow windows [Bsp]
+    Key([mod, 'control'], 'h', lazy.layout.grow_left()),
+    Key([mod, 'control'], 'l', lazy.layout.grow_right()),
+    Key([mod, 'control'], 'j', lazy.layout.grow_down()),
+    Key([mod, 'control'], 'k', lazy.layout.grow_up()),
+
+    # Grow windows [Xmonad]
+    Key([mod], 'i', lazy.layout.grow()),
+    Key([mod], 'm', lazy.layout.shrink()),
+    Key([mod], 'o', lazy.layout.maximize()),
+
+    # Restore size [Xmonad & Bsp]
+    Key([mod], 'n', lazy.layout.normalize()),
+
+    # Toggle between split and unsplit sides [Bsp]
+    Key([mod, 'shift'], 'Return', lazy.layout.toggle_split()),
 
     # Toggle between layouts
     Key([mod], 'Tab', lazy.next_layout()),
@@ -34,11 +60,11 @@ keys = [
     Key([mod], 'Return', lazy.spawn(terminal)),
 
     # Dmenu
-    Key([mod], 'a', lazy.spawn('dmenu_run')),
+    Key([mod], 'd', lazy.spawn('dmenu_run')),
 
     # Menu
-    Key([mod], 'm', lazy.spawn('rofi -show drun')),
-    Key([mod, 'shift'], 'm', lazy.spawn('rofi -show')),
+    Key([mod], 'r', lazy.spawn('rofi -show drun')),
+    Key([mod, 'shift'], 'r', lazy.spawn('rofi -show')),
 
     # Browser
     Key([mod], 'b', lazy.spawn(browser)),
@@ -56,7 +82,7 @@ keys = [
     Key([mod], 'XF86AudioRaiseVolume', lazy.spawn('brightnessctl set +5%')),
 
     # Player
-    Key([mod], 'F9', lazy.spawn('playerctl --player=spotify previous')),
-    Key([mod], 'F10', lazy.spawn('playerctl --player=spotify play-pause')),
-    Key([mod], 'F11', lazy.spawn('playerctl --player=spotify next')),
+    Key([mod], 'F9', lazy.spawn('playerctl previous')),
+    Key([mod], 'F10', lazy.spawn('playerctl play-pause')),
+    Key([mod], 'F11', lazy.spawn('playerctl next')),
 ]
--- .config/qtile/modules/layouts.py
+++ .config/qtile/modules/layouts.py
@@ -6,19 +6,36 @@ from libqtile.config import Match
 from .colors import color
 
 # Layout config
-config = {
-    'border_normal': color[8],
-    'border_focus': color[5],
-    'border_width': 2,
-    'single_border_width': 0,
-    'margin': 9,
-    'single_margin': 9,
-}
+def config(layout_name):
+    config = {
+        'border_normal': color[8],
+        'border_focus': color[5],
+        'border_width': 2,
+        'single_border_width': 0,
+        'margin': 9,
+        'single_margin': 9,
+    }
+    if layout_name == 'bsp':
+        config['fair'] = False
+        config['grow_amount'] = 3
+        config['lower_right'] = False
+
+    elif layout_name == 'xmonad':
+        config['min_ratio'] = 0.30
+        config['max_ratio'] = 0.70
+        config['change_ratio'] = 0.02
+
+    elif layout_name == 'stack':
+        config['border_width'] = 0
+        config['num_stacks'] = 1
+    return config
 
 layouts = [
     # layout.Max(),
-    layout.MonadTall(**config),
-    layout.MonadWide(**config),
+    layout.Bsp(**config('bsp')),
+    layout.MonadTall(**config('xmonad')),
+    layout.MonadWide(**config('xmonad')),
+    layout.Stack(**config('stack')),
 ]
 
 # Floating windows
