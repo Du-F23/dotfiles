6c0402aea94fc0847d233b8abe2bac1cc3274e1a
jx11r <jx11r@hotmail.com>
2022-01-18 23:58:19 -0600

    Fix deprecated scheme

--- .config/nvim/lua/scheme/material.lua
+++ .config/nvim/lua/scheme/material.lua
@@ -2,37 +2,43 @@
 
 -- Global Config
 require('material').setup({
-	contrast = true,
-	borders = false,
-
-	popup_menu = "dark",
+	contrast = {
+		sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+		floating_windows = false, -- Enable contrast for floating windows
+		line_numbers = false, -- Enable contrast background for line numbers
+		sign_column = false, -- Enable contrast background for the sign column
+		cursor_line = false, -- Enable darker background for the cursor line
+		non_current_windows = false, -- Enable darker background for non-current windows
+		popup_menu = false, -- Enable lighter background for the popup menu
+	},
 
 	italics = {
-		comments = false,
-		keywords = false,
-		functions = false,
-		strings = false,
-		variables = false
+		comments = false, -- Enable italic comments
+		keywords = false, -- Enable italic keywords
+		functions = false, -- Enable italic functions
+		strings = false, -- Enable italic strings
+		variables = false -- Enable italic variables
 	},
 
-	contrast_windows = {
-		"terminal",
-		"packer",
-		"qf"
+	contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
+		"terminal", -- Darker terminal background
+		"packer", -- Darker packer background
+		"qf" -- Darker qf list background
 	},
 
-	text_contrast = {
-		lighter = false,
-		darker = false
+	high_visibility = {
+		lighter = false, -- Enable higher contrast text for lighter style
+		darker = false -- Enable higher contrast text for darker style
 	},
 
 	disable = {
-		background = false,
-		term_colors = false,
-		eob_lines = false
+		borders = false, -- Disable borders between verticaly split windows
+		background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
+		term_colors = false, -- Prevent the theme from setting terminal colors
+		eob_lines = false -- Hide the end-of-buffer lines
 	},
 
-	custom_highlights = {}
+	custom_highlights = {} -- Overwrite highlights with your own
 })
 
 vim.g.material_style = 'deep ocean'
