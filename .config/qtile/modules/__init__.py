2a03ee16bd3416277ca5df0260fc50a4f07c6662
jx11r <jx11r@hotmail.com>
2022-01-21 15:47:11 -0600

    Fixed imports

--- .config/qtile/modules/__init__.py
+++ .config/qtile/modules/__init__.py
@@ -1,10 +1,13 @@
 # --==[ Imports ]==--
 
+# Extras
+from .extras.widgets import widget_defaults
+from .extras.widgets import extension_defaults
+from .extras.autostart import autostart
+
+# Core
 from .core.keys import keys
 from .core.groups import groups
 from .core.layouts import layouts, floating_layout
-from .extras.widgets import widget_defaults
-from .extras.widgets import extension_defaults
 from .core.screens import screens
 from .core.mouse import mouse
-from .extras.autostart import autostart
